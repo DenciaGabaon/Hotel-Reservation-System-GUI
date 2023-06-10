24e93b8c161318691d0b9bf2d1fd412052f24d5f
Daphne Camodoc <73266859+DaphneGwyneth@users.noreply.github.com>
2023-06-10 22:01:12 +0800

    Update PanelHotelInformation.java

--- src/loginsyst/PanelHotelInformation.java
+++ src/loginsyst/PanelHotelInformation.java
@@ -1,9 +1,8 @@
 package loginsyst;
 
-import java.awt.Font;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Font;
 import java.awt.Color;
 
 public class PanelHotelInformation extends JPanel {
@@ -18,7 +17,7 @@ public class PanelHotelInformation extends JPanel {
 		
 		
 		JLabel lblThisIsHotelInfo = new JLabel("HOTEL INFORMATION");
-		lblThisIsHotelInfo.setForeground(new Color(0, 0, 128));
+		//lblThisIsHotelInfo.setForeground(new Color(0, 0, 128));
 		lblThisIsHotelInfo.setBounds(189, 36, 205, 19);
 		lblThisIsHotelInfo.setFont(new Font("Dialog", Font.BOLD, 14));
 		add(lblThisIsHotelInfo);

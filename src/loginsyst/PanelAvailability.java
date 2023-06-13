6848738644a970faa21a79206a9b04e739d37f7f
JuanSegoviaVV <juan.segovia@tup.edu.ph>
2023-06-14 00:46:34 +0800

    This is Juan for Tonight

--- src/loginsyst/PanelAvailability.java
+++ src/loginsyst/PanelAvailability.java
@@ -21,6 +21,8 @@ public class PanelAvailability extends JPanel implements ActionListener{
 	private JTextField textField;
 
 	JButton btnNewButton;
+	JComboBox comboBox;
+	JLabel lblNewLabel;
 	public PanelAvailability() {
 		setBackground(new Color(255, 215, 0));
 		
@@ -39,9 +41,10 @@ public class PanelAvailability extends JPanel implements ActionListener{
 		btnNewButton.setBounds(155, 303, 232, 31);
 		add(btnNewButton);
 		
-		JComboBox comboBox = new JComboBox();
+		comboBox = new JComboBox();
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"-SELECT-", "STANDARD", "DELUXE", "SUITE"}));
 		comboBox.setBounds(155, 248, 232, 31);
+		comboBox.addActionListener(comboBox);
 		add(comboBox);
 		
 		JPanel panel = new JPanel();
@@ -72,17 +75,68 @@ public class PanelAvailability extends JPanel implements ActionListener{
 		lblRoomAvailabile.setFont(new Font("Dialog", Font.BOLD, 14));
 		panel_1.add(lblRoomAvailabile);
 		
-		
-		
+		lblNewLabel = new JLabel("NA");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		panel_1.add(lblNewLabel);
+			
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource()==btnNewButton) {
-			//print on the Panel the new 
-			//get string
-			//get room type from combo box
+		if (e.getSource()==btnNewButton && e.getSource()== comboBox) {
+			int roomType = comboBox.getSelectedIndex(); // 1 "STANDARD", 2 is "DELUXE", 3 is "SUITE" 
+			String date = textField.getSelectedText();
+			int available_rooms = 0;
+		//	for (int i = 0; i < 10; i++) // I changed this from num_rooms to MAX_ROOMS, this was the main reason why it won't print more than one
+		//    {
+		//        if (strcmp(rooms[i].type, type) == 0)
+		//        {
+		//            if (strcmp(rooms[i].isReserved, date) != 0)
+		//            {
+		//                available_rooms++;
+		//               }
+		//        }
+		//    }
+		//	available_rooms.ToString(available_rooms);
+		//	lblNewLabel.setText(available_rooms);
 		}
 		
 	}
 }
+
+/*for (int i = 0; i < MAX_ROOMS; i++) // I changed this from num_rooms to MAX_ROOMS, this was the main reason why it won't print more than one
+    {
+       // printf("%s (to show how many rooms)\n", rooms[i].type); //  This is the debugging thing
+        if (strcmp(rooms[i].type, type) == 0)
+        {
+            if (strcmp(rooms[i].isReserved, date) != 0)
+            {
+                available_rooms++;
+               }
+        }
+    }
+    if (available_rooms == 0)
+        printf("\n\t\t\tThere are no available rooms of type %s on the requested date %s\n", type,date);
+    else
+        printf("\n\t\t\t%d Rooms is/are available for type %s.\n", available_rooms, type);
+    getch();
+    
+    
+    void room_Update(char newDate[MAX_DATE_LENGTH], char newType[MAX_TYPE_LENGTH], int roomAmount) // room update for availability
+{
+    int roomFlag = 0;
+    char aab[MAX_DATE_LENGTH] = "NA/NA/NA";
+    for (int i = 0; i < MAX_ROOMS; i++)
+    {
+        if (strcmp(rooms[i].type, newType) == 0)
+        {
+            if (strcmp(rooms[i].isReserved, aab) == 0)
+            {
+                strcpy(rooms[i].isReserved, newDate);
+                roomFlag++;
+                if(roomFlag == roomAmount)
+                    i = MAX_ROOMS;
+            }
+        }
+    }
+}*/
